bf6708a34712112cf97f9efb74a7f61b4857749f
wtvamp <warrenjthompson@gmail.com>
2024-07-30 20:20:51 -0700

    Good ios working - not entirely sure ho

--- android/app/src/main/java/com/dti/nfcapp/MainActivity.kt
+++ android/app/src/main/java/com/dti/nfcapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.kmdevsign.learningNFC
+package com.dti.nfcapp
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/dti/nfcapp/MainApplication.kt
+++ android/app/src/main/java/com/dti/nfcapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.kmdevsign.learningNFC
+package com.dti.nfcapp
 
 import android.app.Application
 import android.content.res.Configuration
